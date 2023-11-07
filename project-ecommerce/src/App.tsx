ef63bcc940b9b4d1a9444982c7aa3b2ccfbe870c
PiachkoAliaksey <alek17y@mail.ru>
2023-11-07 19:54:28 +0300

    add: main FC App

---
+++ project-ecommerce/src/App.tsx
@@ -0,0 +1,61 @@
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import Login from "./pages/Login/Login";
+import Header from "./components/Header/Header";
+import { createBrowserRouter, Outlet, RouterProvider, ScrollRestoration } from "react-router-dom";
+import Footer from "./components/Footer/Footer"
+import { productsData } from "./api/Api";
+import Product from "./components/Product/Product";
+
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <ScrollRestoration/>
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Layout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>,
+        loader:productsData,
+      },
+      {
+        path:'/product/:id',
+        element:<Product/>,
+      },
+      {
+        path:'/cart',
+        element:<Cart/>
+      },
+      {
+        path:'/login',
+        element:<Login/>
+      }
+
+    ]
+  }
+])
+
+function App() {
+
+
+  return (
+    <div className="font-poppins">
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
