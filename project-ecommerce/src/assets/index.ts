cbf6c448bf6d89b221684ba98d7e7a60bf44551b
PiachkoAliaksey <alek17y@mail.ru>
2023-12-02 20:22:56 +0300

    add: entry points index.html main.tsx

--- project-ecommerce/src/assets/index.ts
+++
@@ -1,3 +0,0 @@
-import shoppingBag from './shopping-bag.png';
-import shoppingCart from './shopping-cart.png';
-export {shoppingBag,shoppingCart}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
 import './index.css';
 import { store } from './redux/store.ts';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store.ts'
+import { persistor } from './redux/store.ts';
+import appFirebase from './firebase.config.js'
+
+appFirebase;
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
+  <Provider store={store} >
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
